2e84435067a87d21ef6214c58bad431719eca762
onkar-sopho <onkar21.rai@gmail.com>
2021-10-17 20:03:08 +0530

    Updated the app component template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Basics';
+  pageTitle: string = 'Ace Product Management';
 }
+
